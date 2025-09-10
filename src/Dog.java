ce126af5b33f7c61751d0d956ab73d8a53efa0d5
562591 <562591@my.cuhsd.org>
2025-09-10 11:00:00 -0700

    Added the Dog class

--- src/Dog.java
+++ src/Dog.java
@@ -1,27 +1,24 @@
-import java.util.Locale;
-
+/**
+ * Represents a basic dog.
+ */
 public class Dog {
+    private String speech = "Woof!";
+    private String name = "Clayton";
 
-    //Instance variables
-    private String breed;
-    private String name;
-    private int age;
-
-    //Constructor - next class
-    public Dog(String breed, String name, int age) {
-        this.breed = breed;
-        this.name = name;
-        this.age = age;
+    /**
+     * Dog retrieves specified object
+     * @param thing what was thrown to the dog
+     * @return the object picked up by the dog
+     */
+    public Object fetch(Object thing){
+        return thing;
     }
 
-    //Methods
 
-    //Has the dog move forward
-    public void walk(){}
-
-    //Has the dog walk to a certain location
-    public void walk(Locale stop, String test){}
-
-    //Has the dog walk around a certain location
-    public void walk(String treat, Locale avoid){}
+    /**
+     * Prints the dog's speech to the console.
+     */
+    public void speak(){
+        System.out.println(speech);
+    }
 }
